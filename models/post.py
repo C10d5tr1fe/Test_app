5edf5faaf6a17d76ed53bcc20c454d4d3c9d57ea
CLoud Strife <cloustrife2612@gmail.com>
2019-02-14 22:08:10 +0300

    new commit

--- models/post.py
+++ models/post.py
@@ -11,7 +11,7 @@ class Post(object):
         self.create_date = date
 
     def save_to_mongo(self):
-        Database.insert(collections = 'post', data = self.json())
+        Database.insert(collection = 'post', data = self.json())
 
     def json(self):
         return {
@@ -22,6 +22,17 @@ class Post(object):
             'title':self.title,
             'created_date':self.create_date
         }
+
+    @staticmethod
+    def from_mongo(id):
+        #Post.from_mongo('123')
+        return Database.find_one(collection='posts', query={'id':id})
+
+    @staticmethod
+    def from_blog(id):
+        return [ post for post in Database.find(collection = 'post', query={'blog_id':id})]
+
+
 
 
 
