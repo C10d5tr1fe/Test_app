4ea9b2dc6db7fbf880d69f8aa247c32f8be627b8
C10d5tr1fe <47514630+C10d5tr1fe@users.noreply.github.com>
2019-02-17 18:32:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,15 @@
 from models.database import Database
 from models.post import Post
+from  models.blog import Blog
 
 Database.initialize()
 
+blog = Blog(author='Tosaka',
+            title='Sample title',
+            description = 'Sample description')
 
-post = Post("Post 1 title", "Post 1 content", "Post one author")
-post2 = Post("Post 1 title", "Post 1 content", "Post one author")
+blog.new_post()
+blog.save_to_mongo()
+from_database = Blog.get_from_mongo(blog.id)
+print(blog.get_post())
 
-
-print(post.title)
-print(post2.content)
-
-
-
-
-
-print(12)
-
-
-
-
-
-
-
-"""import pymongo
-
-uri = "mongodb://127.0.0.1:27017"
-client = pymongo.MongoClient(uri)
-database = client['local']
-collection = database['students']
-
-students = [student['name'] for student in collection.find({})]  #list comprehentions
-
-print(students)
-"""
